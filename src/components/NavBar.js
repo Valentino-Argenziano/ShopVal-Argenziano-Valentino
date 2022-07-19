410df737742fc5445bef7314851908b7191d9efc
Valentino Argenziano <valeargenziano@hotmail.com>
2022-07-19 13:42:55 -0300

    Creamos NavBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,14 @@
+function NavBar(){
+    return(
+        <header id="header">
+            <p className="titulo"> Shop Val</p>
+            <nav>
+                <a href="#">Inicio</a>
+                <a href="#">Productos</a>
+                <a href="#">Quienes somos?</a>
+            </nav>
+        </header>
+    )
+}
+
+export default NavBar
