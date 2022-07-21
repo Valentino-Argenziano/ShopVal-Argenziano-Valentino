2a0e4eb6228aeed217abed1429684039ba595c7e
Valentino Argenziano <valeargenziano@hotmail.com>
2022-07-21 16:30:18 -0300

    NavBar personalizado

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import NavBar from "./NavBar"
 
-function App(){
+const App = () => {
     return(
         <>
             <NavBar/>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,36 @@
-function NavBar(){
-    return(
-        <header id="header">
-            <p className="titulo"> Shop Val</p>
-            <nav>
-                <a href="#">Inicio</a>
-                <a href="#">Productos</a>
-                <a href="#">Quienes somos?</a>
-            </nav>
-        </header>
-    )
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import CartWidget from './CartWidget';
+
+
+function CollapsibleExample() {
+    return (
+        <Navbar collapseOnSelect expand="lg" bg="warning" variant="warning">
+            <Container>
+                <Navbar.Brand href="#home" id="title">Shop-Val</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="#features">Inicio</Nav.Link>
+                        <Nav.Link href="#pricing">Productos</Nav.Link>
+                        <NavDropdown title="Contacto" id="collasible-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1">Quienes Somos?</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">
+                                Direccion
+                            </NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Consultas</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                        </NavDropdown>
+                    </Nav>
+                    <Nav>
+                        <CartWidget/>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
 }
 
-export default NavBar
+export default CollapsibleExample;
