d99ba8f8a61bd69aea731b41add151b53c3d7843
Valentino Argenziano <valeargenziano@hotmail.com>
2022-08-22 15:28:50 -0300

    Agregamos formulario al finalizar compra

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -3,12 +3,54 @@ import React, { useContext } from 'react'
 import {contexto} from '../CartContext/CartContext'
 import CartItem from '../CartItem/CartItem'
 import { Link } from 'react-router-dom';
+import {useState} from 'react'
+import  TextField  from '@mui/material/TextField'; 
+import swal from 'sweetalert';
+
+
+
+
 
 
 
 function Cart() {
     const { cart, clearCart, total } = useContext(contexto)
-    console.log(cart);
+
+    const [ openPay, setOpenPay ] = useState(false)
+    const [ name, setName ] = useState("")
+    const [ phone, setPhone ] = useState("")
+    const [ email, setEmail ] = useState("")
+
+
+    function submitOrder(e){
+        e.preventDefault();
+
+        if(cart.length===0){
+            swal({
+                type:"error",
+                title:"Oops...",
+                text:"No hay productos, selecciona alguno!",
+                icon: "error",
+                timer:"2000",
+                width:"50%",
+                padding:"40px",
+                showConfirmButton:false
+            })
+        }else{
+            swal({
+                title:"Compra Exitosa!",
+                text:"Su compra ha sido finalizada, pronto le llegará el pedido.",
+                icon: "success",
+                timer:"2000",
+                width:"50%",
+                padding:"40px",
+                showConfirmButton:false
+            }).then(function(){
+                window.location = "http://localhost:3000/"
+                localStorage.clear()
+            })
+        }
+    }
 
     return (
         <div className="cart">
@@ -39,12 +81,25 @@ function Cart() {
                 <Button onClick={clearCart} variant="contained" color="primary">
                     Sacar todo del carrito
                 </Button>
-                <Button onClick={() => {console.log(cart)}} variant="contained" color="warning">
+                <Button onClick={() => {setOpenPay(true)}} variant="contained" color="warning">
                     A pagar! 
                 </Button>
             </div>
             </>}
 
+
+            { openPay && 
+            <form className='form'noValidate autoComplete="off">
+            <TextField id="standard-basic" label="Nombre" value={name} 
+            onChange={(e) => setName(e.target.value)} />
+            <TextField id="standard-basic" label="Teléfono" value={phone} 
+            onChange={(e) => setPhone(e.target.value)} />
+            <TextField id="standard-basic" label="Email" value={email} 
+            onChange={(e) => setEmail(e.target.value)} />
+            <Button  onClick={submitOrder} variant="contained" color="primary">
+                    Finalizar!
+                </Button> 
+            </form>}
 
         </div>
     )
